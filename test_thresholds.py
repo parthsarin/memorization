2ba350823b3337da596de2eba3004935f313baeb
Parth Sarin <psarin@stanford.edu>
2024-04-05 12:28:37 -0700

    change thresholds

--- test_thresholds.py
+++ test_thresholds.py
@@ -42,33 +42,34 @@ DATA = gen_dataset()
 
 def main(eq_thresh=1e-2):
     m = Model()
-    m.generate_point_mask(0.01)
-    m.mask_point(DATA[0])  # only train 10% of the parameters on this point
-    m.train(DATA, 800)
+    # m.generate_point_mask(0.01)
+    # m.mask_point(DATA[0])  # only train 10% of the parameters on this point
+    m.train(DATA, 300)
 
-    # verify that the model hasn't memorized the params if we mask it
-    print("Predicting using the full model:")
-    with torch.no_grad():
-        print(f"logprob of DATA[0]: {m.logprob(DATA[0])}")
-        print(f"logprob of DATA[1]: {m.logprob(DATA[1])}")
-        print(f"logprob of <s>aaaaaaaaaa</s>: {m.logprob('<s>aaaaaaaaaa</s>')}")
-    print("Hiding the coefficients that saw DATA[0]:")
-    with torch.no_grad():
-        for param, mask in zip(m.model.parameters(), m.mask):
-            param *= 1 - mask
-        print(f"logprob of DATA[0]: {m.logprob(DATA[0])}")
-        print(f"logprob of DATA[1]: {m.logprob(DATA[1])}")
-        print(f"logprob of <s>aaaaaaaaaa</s>: {m.logprob('<s>aaaaaaaaaa</s>')}")
-    m.restore_params()
+    # # verify that the model hasn't memorized the params if we mask it
+    # print("Predicting using the full model:")
+    # with torch.no_grad():
+    #     print(f"logprob of DATA[0]: {m.logprob(DATA[0])}")
+    #     print(f"logprob of DATA[1]: {m.logprob(DATA[1])}")
+    #     print(f"logprob of <s>aaaaaaaaaa</s>: {m.logprob('<s>aaaaaaaaaa</s>')}")
+    # print("Hiding the coefficients that saw DATA[0]:")
+    # with torch.no_grad():
+    #     for param, mask in zip(m.model.parameters(), m.mask):
+    #         param *= 1 - mask
+    #     print(f"logprob of DATA[0]: {m.logprob(DATA[0])}")
+    #     print(f"logprob of DATA[1]: {m.logprob(DATA[1])}")
+    #     print(f"logprob of <s>aaaaaaaaaa</s>: {m.logprob('<s>aaaaaaaaaa</s>')}")
+    # m.restore_params()
 
     thresholds = np.linspace(0, 0.05, 1000)
     seqs = [DATA[0], DATA[1]]
     # get a few out-of-distribution points
     # for entropy in [0, 0.25, 0.75, 1]:
-    #     p = DATA[0]
-    #     while p in DATA:
-    #         p = sample_point(entropy=entropy)
-    #     seqs.append(p)
+    for entropy in [0]:
+        p = DATA[0]
+        while p in DATA:
+            p = sample_point(entropy=entropy)
+        seqs.append(p)
     seq_probs = [[] for _ in range(len(seqs))]
 
     # try to un-memorize seqs[0]
@@ -83,16 +84,16 @@ def main(eq_thresh=1e-2):
         m.restore_params()
 
     # trim the sequences to the point where they're all equal
-    # new_len = min(
-    #     i
-    #     for i in range(len(seq_probs[0]))
-    #     if all(
-    #         np.abs(seq_probs[j][i] - seq_probs[0][i]) < eq_thresh
-    #         for j in range(len(seqs))
-    #     )
-    # )
-    # seq_probs = [seq[: round(new_len * 1.1)] for seq in seq_probs]
-    # thresholds = thresholds[: round(new_len * 1.1)]
+    new_len = min(
+        i
+        for i in range(len(seq_probs[0]))
+        if all(
+            np.abs(seq_probs[j][i] - seq_probs[0][i]) < eq_thresh
+            for j in range(len(seqs))
+        )
+    )
+    seq_probs = [seq[: round(new_len * 1.1)] for seq in seq_probs]
+    thresholds = thresholds[: round(new_len * 1.1)]
 
     # plot the results
     for i, seq in enumerate(seqs):
