653f06e63b1d3f325ade2880372633a16cc50dda
Parth Sarin <psarin@stanford.edu>
2024-04-10 11:25:02 -0700

    learn recall prefix

---
+++ learn_recall_prefix.py
@@ -0,0 +1,168 @@
+"""
+File: learn_recall_prefix.py
+----------------------------
+"""
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch.nn.functional as F
+import torch.nn as nn
+import torch
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+from typing import List, Tuple
+import json
+import random
+import pickle
+
+
+@dataclass_json
+@dataclass
+class PrefixLearnerLog:
+    prefix_len: int
+    closest_prefix_str: str
+    loss: float
+    avg_dist: float
+    generation: str
+    target: str
+
+
+class PrefixLearner:
+    def __init__(self, model_name):
+        self.model = AutoModelForCausalLM.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+        self.embeddings = self.model.transformer.wte
+        self.embedding_dim = self.embeddings.embedding_dim
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+    def find_nearest_token(self, embedding):
+        # find the nearest token to the embedding
+        token_embeddings = self.embeddings.weight
+        distances = torch.cdist(embedding, token_embeddings)
+        return distances.argmin(), distances.min().item()
+
+    def learn_prefix(
+        self,
+        target,
+        min_recall_tokens=1,
+        max_recall_tokens=20,
+        verbose=True,
+        epochs_per_pf_len=10_000,
+    ) -> Tuple[List[torch.Tensor], List[PrefixLearnerLog]]:
+        # get the embeddings of the target
+        raw_target = target
+        target_tokens = self.tokenizer(target, return_tensors="pt")["input_ids"]
+        max_recall_tokens = min(max_recall_tokens, target_tokens.size(1) + 4)
+        target = self.embeddings(target_tokens)
+
+        log = []
+        embeddings = []
+
+        for prefix_len in range(min_recall_tokens, max_recall_tokens + 1):
+            prefix = torch.randn(1, prefix_len, self.embedding_dim)
+            prefix = nn.Parameter(prefix)
+
+            # learn the prefix with backpropagation
+            opt = torch.optim.Adam([prefix], lr=1e-3)
+            for _ in range(epochs_per_pf_len):
+                opt.zero_grad()
+                seq = torch.cat([prefix, target], dim=1)
+                logits = self.model(inputs_embeds=seq).logits
+
+                # predictions from the end of the prefix to the end of the target
+                logits = logits[:, prefix_len - 1 : -1, :]
+
+                # cross-entropy loss
+                loss = F.cross_entropy(
+                    logits.view(-1, logits.size(-1)), target_tokens.view(-1)
+                )
+
+                loss.backward()
+                opt.step()
+
+            # convert the prefix to tokens
+            nearest = []
+            dists = []
+            for i in range(prefix_len):
+                tok, dist = self.find_nearest_token(prefix[0, i].unsqueeze(0))
+                nearest.append(tok)
+                dists.append(dist)
+            prefix_decoded = self.tokenizer.decode(
+                torch.LongTensor(nearest), skip_special_tokens=True
+            )
+            avg_dist = sum(dists) / len(dists)
+
+            # generate the rest of the sequence
+            generation = self.model.generate(
+                inputs_embeds=prefix,
+                attention_mask=torch.ones(1, prefix_len),
+                max_length=prefix_len + target_tokens.size(1),
+                num_return_sequences=1,
+                do_sample=False,
+            )
+            generation = generation[0, prefix_len:]  # remove prefix
+            generation = self.tokenizer.batch_decode(
+                generation, skip_special_tokens=True
+            )[0]
+
+            log.append(
+                PrefixLearnerLog(
+                    prefix_len=prefix_len,
+                    closest_prefix_str=prefix_decoded,
+                    loss=loss.item(),
+                    avg_dist=avg_dist,
+                    generation=generation,
+                    target=raw_target,
+                )
+            )
+            embeddings.append(prefix)
+
+            if verbose:
+                # show the loss and the closest prefix
+                print(
+                    f"[prefix len {prefix_len}] loss: {loss.item():.4f}, closest prefix: {repr(prefix_decoded)} (avg dist {avg_dist:.4f}), generation: {repr(generation)}, target: {repr(raw_target)}"
+                )
+
+        return embeddings, log
+
+
+if __name__ == "__main__":
+    pl = PrefixLearner("EleutherAI/gpt-neo-125m")
+    samples = json.load(open("pile_samples.json")) + json.load(
+        open("non_pile_samples.json")
+    )
+    random.shuffle(samples)
+
+    learning_logs = []
+    embeddings_logs = {}
+
+    for sample in samples:
+        prefix = sample["prefix"]
+        target = sample["expected"]
+        em = sample["em"]
+        origin = sample["origin"]
+        greedy_completion = sample["completion"]
+
+        print("-" * 80)
+        print(f"Learning prefix for target: {repr(target)}")
+        print(f"* Prefix: {repr(prefix)}")
+        print(f"* EM: {em} (greedy generation: {repr(greedy_completion)})")
+        print(f"* Origin: {origin}")
+        print()
+
+        embeddings, log = pl.learn_prefix(target, verbose=True)
+        learning_logs.append(
+            {
+                **sample,
+                "learning_log": [l.to_dict() for l in log],
+            }
+        )
+        json.dump(learning_logs, open("learning_log.json", "w"), indent=2)
+
+        embeddings_logs[(prefix, target)] = embeddings
+        with open("embeddings_log.pkl", "wb") as f:
+            pickle.dump(embeddings_logs, f)
+
+        print()
+        print("-" * 80)
