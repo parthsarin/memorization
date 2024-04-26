415ef0b9f47f535aad3cc3156ce488f095a9fd16
Parth Sarin <psarin@stanford.edu>
2024-04-25 21:43:23 -0700

    reorganize

--- visualize.py
+++ visualize.py
@@ -4,11 +4,11 @@ import seaborn as sns
 import json
 import collections
 
-sns.set_theme(style='whitegrid')
+sns.set_theme(style="whitegrid")
 plt.ion()
 
 # get the data from the logs
-learning_logs = json.load(open("learning_log.json"))
+learning_logs = json.load(open("learning_log_2_7b.json"))
 
 # best loss for each EM
 best_losses = collections.defaultdict(list)
@@ -17,16 +17,16 @@ pile_losses = collections.defaultdict(list)
 non_pile_losses = collections.defaultdict(list)
 ems = [[], []]
 for log in learning_logs:
-    em = log['em']
-    losses = [l['loss'] for l in log['learning_log']]
-    pls = [l['prefix_len'] for l in log['learning_log']]
-    origin = log['origin']
+    em = log["em"]
+    losses = [l["loss"] for l in log["learning_log"]]
+    pls = [l["prefix_len"] for l in log["learning_log"]]
+    origin = log["origin"]
 
     best_loss_idx = np.argmin(losses)
 
     best_losses[em].append(losses[best_loss_idx])
     best_pls[em].append(pls[best_loss_idx])
-    if origin == 'pile':
+    if origin == "pile":
         add_to = pile_losses
         # if em > 8:
         #     continue
@@ -36,8 +36,8 @@ for log in learning_logs:
         add_to[pl].append(loss)
 
 
-print(f'Average pile EM: {np.mean(ems[0])}, std: {np.std(ems[0])}')
-print(f'Average non-pile EM: {np.mean(ems[1])}, std: {np.std(ems[1])}')
+print(f"Average pile EM: {np.mean(ems[0])}, std: {np.std(ems[0])}")
+print(f"Average non-pile EM: {np.mean(ems[1])}, std: {np.std(ems[1])}")
 
 for k, v in best_losses.items():
     best_losses[k] = (np.mean(v), np.std(v))
@@ -52,10 +52,10 @@ k = np.array(list(best_losses.keys()))
 v = np.array(list(best_losses.values()))
 idx = np.argsort(k)
 
-plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker='o', label='best loss')
+plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="best loss")
 
-plt.xlabel('EM')
-plt.ylabel('loss')
+plt.xlabel("EM")
+plt.ylabel("loss")
 plt.show()
 
 # line plot the best prefix length for each EM
@@ -65,10 +65,10 @@ k = np.array(list(best_pls.keys()))
 v = np.array(list(best_pls.values()))
 idx = np.argsort(k)
 
-plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker='o', label='best prefix length')
+plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="best prefix length")
 
-plt.xlabel('EM')
-plt.ylabel('prefix length')
+plt.xlabel("EM")
+plt.ylabel("prefix length")
 plt.show()
 
 # plot the loss in the pile and non-pile samples
@@ -81,16 +81,16 @@ k = np.array(list(pile_losses.keys()))
 v = np.array(list(pile_losses.values()))
 idx = np.argsort(k)
 
-plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker='o', label='pile')
+plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="pile")
 
 k = np.array(list(non_pile_losses.keys()))
 v = np.array(list(non_pile_losses.values()))
 idx = np.argsort(k)
 
-plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker='o', label='non-pile')
+plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="non-pile")
 
-plt.xlabel('prefix length')
-plt.ylabel('loss')
+plt.xlabel("prefix length")
+plt.ylabel("loss")
 plt.legend()
 plt.show()
 breakpoint()
