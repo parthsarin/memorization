e0c140fccc98c8af8e03bb5d5c89c24899c35fa3
Parth Sarin <psarin@stanford.edu>
2024-05-23 18:51:44 -0700

    sentence embeddings

---
+++ sentence_embeddings.py
@@ -0,0 +1,74 @@
+"""
+File: sentence_embeddings.py
+----------------------------
+
+Compute sentence embeddings using the prefix learner and try it out on the
+sentence similarity task.
+"""
+from datasets import load_dataset
+import argparse
+import torch
+import wandb
+from learn_recall_prefix import PrefixLearner
+from tqdm import tqdm
+import json
+
+def main(args):
+    data = load_dataset("embedding-data/SPECTER")
+    learner = PrefixLearner(args.model)
+    data = data["train"]
+
+    out = []
+
+    # compute sentence embeddings
+    for i in tqdm(range(len(data))):
+        x = data[i]["set"]
+        query, pos, neg = x
+
+        # get embeddings for each
+        emb, ll = learner.learn_prefix(query, min_recall_tokens=1, max_recall_tokens=1)
+        query_emb = emb[-1]
+        query_logprob = ll[-1].logprob
+
+        emb, ll = learner.learn_prefix(pos, min_recall_tokens=1, max_recall_tokens=1)
+        pos_emb = emb[-1]
+        pos_logprob = ll[-1].logprob
+
+        emb, ll = learner.learn_prefix(neg, min_recall_tokens=1, max_recall_tokens=1)
+        neg_emb = emb[-1]
+        neg_logprob = ll[-1].logprob
+
+        # normalize embeddings
+        query_emb = query_emb / torch.norm(query_emb)
+        pos_emb = pos_emb / torch.norm(pos_emb)
+        neg_emb = neg_emb / torch.norm(neg_emb)
+
+        # compute cosine similarity
+        pos_sim = torch.dot(query_emb, pos_emb)
+        neg_sim = torch.dot(query_emb, neg_emb)
+
+        record = {
+            "query": query,
+            "pos": pos,
+            "neg": neg,
+            "query_logprob": query_logprob.item(),
+            "pos_logprob": pos_logprob.item(),
+            "neg_logprob": neg_logprob.item(),
+            "pos_sim": pos_sim.item(),
+            "neg_sim": neg_sim.item(),
+        }
+
+        wandb.log(record)
+        out.append(record)
+
+        with open(args.out, "w") as f:
+            json.dump(out, f, indent=2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, default="EleutherAI/gpt-neo-2.7B")
+    parser.add_argument("--out", type=str, default="sentence_embeddings.json")
+    args = parser.parse_args()
+
+    main(args)
--- visualize.py
+++ visualize.py
@@ -12,93 +12,96 @@ plt.ion()
 learning_logs = json.load(open("out/learning_log_125m.json"))
 
 # best loss for each EM
-best_losses = collections.defaultdict(list)
+best_logprobs = collections.defaultdict(list)
 best_pls = collections.defaultdict(list)
-pile_losses = collections.defaultdict(list)
-non_pile_losses = collections.defaultdict(list)
+pile_logprobs = collections.defaultdict(list)
+non_pile_logprobs = collections.defaultdict(list)
+pl_1_logprobs = collections.defaultdict(list)
 pile_ranges = []
 non_pile_ranges = []
 ems = [[], []]
 for log in learning_logs:
     em = log["em"]
-    losses = [l["loss"] for l in log["learning_log"]]
+    logprobs = [l["logprob"] for l in log["learning_log"]]
     pls = [l["prefix_len"] for l in log["learning_log"]]
     origin = log["origin"]
 
-    range = np.max(losses) - np.min(losses)
+    range = np.max(logprobs) - np.min(logprobs)
 
-    best_loss_idx = np.argmin(losses)
+    best_logprob_idx = np.argmax(logprobs)
 
-    best_losses[em].append(losses[best_loss_idx])
-    best_pls[em].append(pls[best_loss_idx])
+    best_logprobs[em].append(logprobs[best_logprob_idx])
+    best_pls[em].append(pls[best_logprob_idx])
+    pl_1_logprobs[em].append(logprobs[0])
     if origin == "pile":
-        add_to = pile_losses
+        add_to = pile_logprobs
         pile_ranges.append(range)
-        # if em > 8:
-        #     continue
     else:
-        add_to = non_pile_losses
+        add_to = non_pile_logprobs
         non_pile_ranges.append(range)
-    for pl, loss in zip(pls, losses):
-        add_to[pl].append(loss)
+    for pl, logprob in zip(pls, logprobs):
+        add_to[pl].append(logprob)
 
 
 print(f"Average pile EM: {np.mean(ems[0])}, std: {np.std(ems[0])}")
 print(f"Average non-pile EM: {np.mean(ems[1])}, std: {np.std(ems[1])}")
 
-for k, v in best_losses.items():
-    best_losses[k] = (np.mean(v), np.std(v))
+for k, v in best_logprobs.items():
+    best_logprobs[k] = (np.mean(v), np.std(v))
+
+for k, v in pl_1_logprobs.items():
+    pl_1_logprobs[k] = (np.mean(v), np.std(v))
 
 for k, v in best_pls.items():
     best_pls[k] = (np.mean(v), np.std(v))
 
-# line plot the best loss for each EM
+# line plot the best logprob for each EM
 plt.figure(figsize=(10, 6))
 
-k = np.array(list(best_losses.keys()))
-v = np.array(list(best_losses.values()))
+k = np.array(list(pl_1_logprobs.keys()))
+v = np.array(list(pl_1_logprobs.values()))
 idx = np.argsort(k)
 
-plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="best loss")
+plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="best logprob")
 
 plt.xlabel("EM")
-plt.ylabel("loss")
+plt.ylabel("logprob")
 plt.savefig("em_pl_loss.png", dpi=300)
 plt.show()
 
 # line plot the best prefix length for each EM
+# plt.figure(figsize=(10, 6))
+
+# k = np.array(list(best_pls.keys()))
+# v = np.array(list(best_pls.values()))
+# idx = np.argsort(k)
+
+# plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="best prefix length")
+
+# plt.xlabel("EM")
+# plt.ylabel("prefix length")
+# plt.show()
+
+# plot the logprob in the pile and non-pile samples
 plt.figure(figsize=(10, 6))
 
-k = np.array(list(best_pls.keys()))
-v = np.array(list(best_pls.values()))
-idx = np.argsort(k)
+pile_logprobs = {k: (np.mean(v), np.std(v)) for k, v in pile_logprobs.items()}
+non_pile_logprobs = {k: (np.mean(v), np.std(v)) for k, v in non_pile_logprobs.items()}
 
-plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="best prefix length")
-
-plt.xlabel("EM")
-plt.ylabel("prefix length")
-plt.show()
-
-# plot the loss in the pile and non-pile samples
-plt.figure(figsize=(10, 6))
-
-pile_losses = {k: (np.mean(v), np.std(v)) for k, v in pile_losses.items()}
-non_pile_losses = {k: (np.mean(v), np.std(v)) for k, v in non_pile_losses.items()}
-
-k = np.array(list(pile_losses.keys()))
-v = np.array(list(pile_losses.values()))
+k = np.array(list(pile_logprobs.keys()))
+v = np.array(list(pile_logprobs.values()))
 idx = np.argsort(k)
 
 plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="pile")
 
-k = np.array(list(non_pile_losses.keys()))
-v = np.array(list(non_pile_losses.values()))
+k = np.array(list(non_pile_logprobs.keys()))
+v = np.array(list(non_pile_logprobs.values()))
 idx = np.argsort(k)
 
 plt.errorbar(k[idx], v[idx, 0], yerr=v[idx, 1], marker="o", label="non-pile")
 
 plt.xlabel("prefix length")
-plt.ylabel("loss")
+plt.ylabel("logprob")
 plt.legend()
 plt.show()
 
