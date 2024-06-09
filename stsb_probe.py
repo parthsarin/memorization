5a357e02cf61a81091f007bc9cf11e7c255b4c97
Parth Sarin <psarin@stanford.edu>
2024-06-09 12:11:41 -0700

    stsb probe

---
+++ stsb_probe.py
@@ -0,0 +1,148 @@
+"""
+File: stsb_probe.py
+-------------------
+
+Learns a probe on the embedding data to see if we can reconstruct semantic
+information from the embeddings.
+"""
+import argparse
+import torch
+import torch.nn as nn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class Probe(nn.Module):
+    def __init__(self, embedding_dim):
+        super().__init__()
+        self.m = nn.Sequential(
+            nn.Linear(embedding_dim, embedding_dim),
+            nn.ReLU(),
+            nn.Linear(embedding_dim, embedding_dim),
+            nn.ReLU(),
+            nn.Linear(embedding_dim, embedding_dim),
+        )
+
+    def forward(self, x):
+        x = self.m(x)
+        return x / torch.norm(x, dim=-1, keepdim=True)
+
+
+def main(args):
+    embeddings = torch.load(args.input_file)
+    embeddings = [
+        (*k, *v) for k, v in embeddings.items()
+    ]  # (s1, s2, s1_emb, s2_emb, score)
+    embedding_dim = embeddings[0][2].shape[0]
+
+    probe = Probe(embedding_dim).to(device)
+    optimizer = torch.optim.Adam(probe.parameters(), lr=args.learning_rate)
+    loss_fn = nn.MSELoss(reduction="sum")
+
+    # trian/test split
+    split = int(0.8 * len(embeddings))
+    train_data = embeddings[:split]
+    test_data = embeddings[split:]
+
+    for epoch in range(args.epochs):
+        probe.train()
+        train_loss = 0
+        for i in range(0, len(train_data), args.batch_size):
+            batch = train_data[i : i + args.batch_size]
+            s1, s2, s1_emb, s2_emb, score = zip(*batch)
+            s1_emb = torch.stack(s1_emb).to(device)
+            s2_emb = torch.stack(s2_emb).to(device)
+            score = torch.tensor(score).to(device)
+
+            optimizer.zero_grad()
+            s1_emb = probe(s1_emb)
+            s2_emb = probe(s2_emb)
+
+            # take their dot product
+            score_pred = torch.sum(s1_emb * s2_emb, dim=-1)
+            loss = loss_fn(score_pred, score)
+
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+
+            wandb.log(
+                {
+                    "batch_loss": loss.item() / len(batch),
+                    "epoch": epoch,
+                }
+            )
+
+        train_loss /= len(train_data)
+
+        # test
+        probe.eval()
+        test_loss = 0
+        for i in range(0, len(test_data), args.batch_size):
+            batch = test_data[i : i + args.batch_size]
+            s1, s2, s1_emb, s2_emb, score = zip(*batch)
+            s1_emb = torch.stack(s1_emb).to(device)
+            s2_emb = torch.stack(s2_emb).to(device)
+            score = torch.tensor(score).to(device)
+
+            s1_emb = probe(s1_emb)
+            s2_emb = probe(s2_emb)
+
+            # take their dot product
+            score_pred = torch.sum(s1_emb * s2_emb, dim=-1)
+            loss = loss_fn(score_pred, score)
+
+            test_loss += loss.item()
+
+        test_loss /= len(test_data)
+
+        print(f"[epoch {epoch}] train_loss: {train_loss}, test_loss: {test_loss}")
+        wandb.log({"train_loss": train_loss, "test_loss": test_loss, "epoch": epoch})
+
+        # checkpoint
+        torch.save(probe.state_dict(), args.output_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Learns a probe on the embedding data to see if we can reconstruct semantic information from the embeddings."
+    )
+
+    parser.add_argument(
+        "input_file",
+        type=str,
+        help="The file containing the embeddings.",
+        default="out/stsb_embeddings.pkl",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output_file",
+        type=str,
+        help="The file to save the probe to.",
+        default="out/stsb_probe.pkl",
+    )
+
+    parser.add_argument(
+        "-b", "--batch_size", type=int, help="The batch size to use.", default=50
+    )
+
+    parser.add_argument(
+        "-n",
+        "--epochs",
+        type=int,
+        help="The number of epochs to train for.",
+        default=1000,
+    )
+
+    parser.add_argument(
+        "-l",
+        "--learning_rate",
+        type=float,
+        help="The learning rate to use.",
+        default=1e-3,
+    )
+
+    args = parser.parse_args()
+    main(args)
--- visualize/qa.py
+++ visualize/qa.py
@@ -1,11 +1,12 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
-import json
 import numpy as np
+import json
 
 sns.set_theme(style="whitegrid")
 
-qa_log = json.load(open("out/qa_log.json"))
+qa_log = json.load(open("../out/qa_log_EleutherAI_gpt-neo-2.7B.json"))
+# qa_log = json.load(open("../out/qa_log_mistralai_Mistral-7B-v0.1.json"))
 print(f"Loaded {len(qa_log)} logs")
 
 
@@ -14,20 +15,20 @@ def metric(log):
     # how should we normalize these based on the length of the question?
     best_logprob = max(d["logprob"] for d in log)
     worst_logprob = min(d["logprob"] for d in log)
-    thresh = worst_logprob + 0.6 * (best_logprob - worst_logprob)
-    reaches_target = [d["prefix_len"] for d in log if d["logprob"] > thresh]
-    return min(reaches_target) if reaches_target else float("inf")
-    # return best_logprob - worst_logprob
+    # thresh = worst_logprob + 0.6 * (best_logprob - worst_logprob)
+    # reaches_target = [d["prefix_len"] for d in log if d["logprob"] > thresh]
+    # return min(reaches_target) if reaches_target else float("inf")
+    return best_logprob - worst_logprob
 
 
-y = [d["correct_answer_logit"] for d in qa_log]
+y = [d["correct_answer_logprob"] for d in qa_log]
 x = [metric(d["extraction_log"]) for d in qa_log]
 
 plt.figure(figsize=(12, 6))
 # sns.scatterplot(x=x, y=y)
 # errorbar
-sns.regplot(x=x, y=y, x_estimator=np.mean, x_ci="sd")
+sns.regplot(x=x, y=y, x_estimator=np.mean, x_ci="ci")
 plt.xlabel("Extraction metric")
-plt.ylabel("Correct answer logit")
-plt.title("Extraction metric vs correct answer logit")
+plt.ylabel("Correct answer logprob")
+plt.title("Extraction metric vs correct answer logprob")
 plt.show()
